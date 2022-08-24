36838a9f090d177a0c675c42389898e26ce62f57
JavApext <JavApext@gmail.com>
2022-08-24 13:17:59 +0900

    2022/08/24 제3장 데이터베이스 관리, 제4장 SQL 고급 수업예제

---
+++ 제3장 데이터베이스 관리.sql
@@ -0,0 +1,19 @@
+#날짜 : 2022/08/24
+#이름 : 길은섭
+#내용 : 제3장 데이터베이스 관리
+
+#실습 3-1 소문자 반영 안되서 _(스네이크) 로 표기함, %는 호스트 주소
+create database `java1_Test_DB`;
+create user 'java1_test'@'%' identified by '1234';
+grant all privileges on `java1_test_db`.* to 'java1_test'@'%';
+flush privileges;
+
+#실습 3-2
+#실습 3-3
+#실습 3-4
+#실습 3-5
+#실습 3-6
+#실습 3-7
+#실습 3-8
+
+
